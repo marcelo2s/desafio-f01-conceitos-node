029bb3b30fbce513eb2305170f1db051875685b7
Marcelo Junior <kronussigma@gmail.com>
2020-04-21 21:25:11 -0300

    Created all operations

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -11,23 +11,73 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.status(200).json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+  const id = uuid();
+  const likes = 0; 
+
+  const repository = { id, title, url, techs, likes };
+
+  repositories.push(repository);
+
+  return response.status(201).json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+
+  const index = getElementPositionById(repositories, id);
+  
+  if(index > -1) {
+    repositories[index]['title'] = title;
+    repositories[index]['url'] = url;
+    repositories[index]['techs'] = techs;
+
+    return response.status(200).json(repositories[index]);
+  }
+  
+  return response.status(400).json();
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const index = getElementPositionById(repositories, id);
+
+  if(index > -1) {
+    repositories.splice(index, 1);
+
+    return response.status(204).json();
+  } 
+    
+  return response.status(400).json();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const index = getElementPositionById(repositories, id);
+
+  if(index > -1) {
+    repositories[index]['likes']++;
+
+    return response.status(200).json(repositories[index]);
+  }
+  return response.status(400).json();
 });
+
+const getElementPositionById = (collection, id) => {
+  let position = null;
+
+  collection.filter((item, index) => {
+    if(item.id === id) position = index;
+  });
+
+  return position || position === 0 ? position : -1;
+};
 
 module.exports = app;
